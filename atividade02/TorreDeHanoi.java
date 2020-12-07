57edf459c07029f18d5be25be29dd3d6bba44801
Marcos Antônio Baccin <74544990+marcosbaccin@users.noreply.github.com>
2020-12-07 15:36:56 -0300

    Add files via upload

--- atividade02/TorreDeHanoi.java
+++ atividade02/TorreDeHanoi.java
@@ -1,17 +1,17 @@
 package ed2.atividade02;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Scanner;
 
 public class TorreDeHanoi {
 
+	static long numMovimentos;
+	
 	static void registrar(int origem, int destino) {
 		
-		System.out.println(origem + " -> " + destino);
-	}
-	
-	static void calcularQtdDeMovimentos(int numDiscos) {
-		int qtd = (int) (Math.pow(2, numDiscos) - 1);
-		System.out.println("Quantidade de movimentos: " + qtd);
+		numMovimentos++;
+		System.out.println("[" + numMovimentos + "]: " + origem + " -> " + destino);
 	}
 
 	static void movimentar(int numDiscos, int origem, int destino, int trabalho) {
@@ -31,8 +31,18 @@ public class TorreDeHanoi {
 		System.out.println("Digite o número de discos: ");
 		numDiscos = scan.nextInt();
 		
+		Instant tempoInicial = Instant.now();
+		
 		movimentar(numDiscos, 1, 3, 2);
-		calcularQtdDeMovimentos(numDiscos);
+		
+		Instant tempoFinal = Instant.now();
+		
+		Duration  tempoDecorrido = Duration.between(tempoInicial, tempoFinal);
+		
+		String tempo = tempoDecorrido.toHoursPart()+":"+tempoDecorrido.toMinutesPart()+":"
+						+tempoDecorrido.toSecondsPart()+":"+tempoDecorrido.toMillisPart();
+		
+		System.out.println("Tempo de execução para " + numDiscos + " discos: " + tempo);
 		
 		scan.close();
 	}
